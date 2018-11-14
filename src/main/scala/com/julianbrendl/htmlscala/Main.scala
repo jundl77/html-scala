c63e2c32264c1fd4b27529034931937fa583aa94
Julian <julianbrendl@gmail.com>
2018-11-14 20:43:30 +0100

    fixed localhost

--- src/main/scala/com/julianbrendl/htmlscala/Main.scala
+++ src/main/scala/com/julianbrendl/htmlscala/Main.scala
@@ -13,7 +13,7 @@ object Main extends StreamApp {
   val port: Int = envOrNone("HTTP_PORT").fold(3001)(_.toInt)
 
   /** Start the server */
-  def stream(args: List[String]): fs2.Stream[Task, Nothing] = BlazeBuilder.bindHttp(port)
+  def stream(args: List[String]): fs2.Stream[Task, Nothing] = BlazeBuilder.bindHttp(port, host="0.0.0.0")
     .mountService(Router.transpile, "/")
     .serve
 }
